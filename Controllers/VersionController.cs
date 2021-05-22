47b37d9723c0f2a2a5806e8b83ba22f3386ae47e
Natalya <muratovan0921@gmail.com>
2021-05-22 11:20:31 +0700

    New fixes in Version Controller

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var versionInfo = new Version
+            {
+                Company = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company,
+                Product = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyProductAttribute>().Product,
+                ProductVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion
+            };
+
+            return Ok(versionInfo);
+        }
+    }
+}
